a7144502a770aad3f24f077ef61d84a3fdf9d21c
sirkells <kelechi_igbokwe@yahoo.com>
2019-03-15 12:29:27 +0100

    change route

--- src/data/socialProfile.js
+++ src/data/socialProfile.js
@@ -1,7 +1,7 @@
-import emailIcon from "./assets/email_icon.png";
-import githubIcon from "./assets/github_icon.png";
-import linkedinIcon from "./assets/linkedin_icon.png";
-import twitterIcon from "./assets/twitter_icon.png";
+import emailIcon from "../assets/email_icon.png";
+import githubIcon from "../assets/github_icon.png";
+import linkedinIcon from "../assets/linkedin_icon.png";
+import twitterIcon from "../assets/twitter_icon.png";
 
 const SOCIAL_PROFILES = [
   {
