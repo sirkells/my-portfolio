f5045e260019d6b883042ab51907dc68215e39a8
sirkells <kelechi_igbokwe@yahoo.com>
2019-03-15 11:55:37 +0100

    Updates

---
+++ precache-manifest.1979e018823a40a2f4fc060b004c51ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b20d20c9a5869f78595",
+    "url": "/my-portfolio/static/css/main.76c506e0.chunk.css"
+  },
+  {
+    "revision": "0b20d20c9a5869f78595",
+    "url": "/my-portfolio/static/js/main.e53246c5.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/my-portfolio/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a786b17f6829e25df396650e76ac3d6b",
+    "url": "/my-portfolio/index.html"
+  }
+];
