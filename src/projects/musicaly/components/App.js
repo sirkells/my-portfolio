d016d6b2d32e001ddd23b7f71da7d1c2222fdc1c
sirkells <kelechi_igbokwe@yahoo.com>
2019-03-31 17:22:28 +0200

    refactored app component

--- src/projects/musicaly/components/App.js
+++ src/projects/musicaly/components/App.js
@@ -8,13 +8,18 @@ const URL_ADDRESS = "https://spotify-api-wrapper.appspot.com";
 const DEFAULT_ARTIST = "Coldplay";
 const Background =
   "http://wallpoper.com/images/00/24/09/32/abstract-music_00240932.jpg";
-
 class App extends Component {
-  state = { artistData: null, topTracks: [], notLoaded: true };
+  state = { artistData: null, topTracks: [], Notloaded: true };
 
   componentDidMount() {
     this.searchArtist(DEFAULT_ARTIST);
   }
+  artistName = () => {
+    if (this.state.Notloaded) {
+      return DEFAULT_ARTIST;
+    }
+    return this.state.artistData.artist.name;
+  };
   searchArtist = searchQuery => {
     // fetch artist data using searchQuery gotten from search component
     fetch(`${URL_ADDRESS}/artist/${searchQuery}`)
@@ -23,7 +28,8 @@ class App extends Component {
         // check if the artist in searchQuery exist in the api
         if (res.artists.total > 0) {
           const artistData = res.artists.items[0];
-          this.setState({ artistData: artistData, notLoaded: false });
+          console.log(artistData);
+          this.setState({ artistData: artistData, Notloaded: false });
 
           // fetches top track of artist based on artist id
           fetch(`${URL_ADDRESS}/artist/${artistData.id}/top-tracks`)
@@ -35,21 +41,23 @@ class App extends Component {
       .catch(err => alert(err.message));
   };
   render() {
-    const { artistData, topTracks, notLoaded } = this.state;
+    const { artistData, topTracks, Notloaded } = this.state;
+
     return (
       <div
         style={{
           backgroundImage: `url(${
-            notLoaded ? Background : artistData.images[0].url
+            Notloaded ? Background : artistData.images[0].url
           })`,
           backgroundSize: "cover",
           overflow: "hidden"
         }}
       >
         <Header />
-
+        <h1>Musicaly</h1>
         <Search searchArtist={this.searchArtist} />
         <Artist artist={artistData} />
+        <h1>{this.artistName} Top Ten Tracks</h1>
         <Tracks tracks={topTracks} />
       </div>
     );
